5429f0de512e3b53be4304c21603b1043dcce96f
padaelocomunity2 <sahrulalfa45@gmail.com>
2021-09-19 08:13:10 +0800

    add:builder.php

--- Builder/Builder.php
+++ Builder/Builder.php
@@ -4,34 +4,100 @@ interface BuilderRumah{
     public function setJendela(int $jendela);
     public function setAtap(string $atap);
     public function setPintu(string $pintu);
+    public function build();
 }
 
 class RumahKayu implements BuilderRumah{
+
+    private $jendela;
+    private $atap;
+    private $pintu;
+    private $tangga;
     public function setJendela(int $jendela)
     {
-        
+        $this->jendela =$jendela;
+        return $this;
     }
     public function setAtap( string $atap)
     {
-
+        $this->atap = $atap;
+        return $this;
     }
     public function setPintu(string $pintu)
     {
+        $this->pintu = $pintu;
+        return $this;
+    }
 
+    public function settTangga(bool $tangga)
+    {
+        $this->tangga = $tangga;
+        return $this;
+    }
+
+    public function build()
+    {
+        echo $this->jendela.$this->atap.$this->pintu.$this->tangga;
     }
 }
 
 class RumahKaca implements BuilderRumah{
+
+    private $jendela;
+    private $atap;
+    private $pintu;
+    private $kolam;
+
     public function setJendela(int $jendela)
     {
-
+        $this->jendela =$jendela;
+        return $this;
     }
     public function setAtap( string $atap)
     {
-
+        $this->atap = $atap;
+        return $this;
     }
     public function setPintu(string $pintu)
     {
+        $this->pintu = $pintu;
+        return $this;
+    }
 
+    public function kolamRenang(bool $kolam)
+    {
+        $this->kolam = $kolam;
+        return $this;
+    }
+
+    public function build()
+    {
+        echo $this->jendela.$this->atap.$this->pintu.$this->kolam;
     }
 }
+
+
+
+class ClientCode{
+    
+    private $buildRumah;
+    public function __construct(BuilderRumah $builderRumah)
+    {
+        $this->buildRumah = $builderRumah;
+    }
+
+    public function BangunRumah()
+    {
+        $this->buildRumah->build();
+    }
+}
+
+
+
+$rumahKayu = new RumahKayu();
+
+$rumahKayu->setJendela(2)->setAtap("semen")->setPintu("kayu");
+
+$clicentCode = new ClientCode($rumahKayu);
+
+$clicentCode->BangunRumah();
